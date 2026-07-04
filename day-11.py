6c89e666944120a655cddfa2e5cae106602f20a3
SANJAY ROCKY <sanjay43650@gmail.com>
2026-07-04 13:07:23 +0530

    Add files via upload

---
+++ day-11.py
@@ -0,0 +1,97 @@
+"""
+palindrom
+=========
+eg
+--
+n = "sanjay rocky"
+empty = " "
+for i in n:
+    empty = i + empty
+if empty == so:
+    print(f"{n} is a palindrom"}
+else:
+    print(f"{n} is  not a palindrom"}
+
+eg2
+---
+n = str(input("enter name: "))
+a = n[::-1]
+if a == n:
+    print(f"{n} is a palindrom")
+else:
+    print(f"{n} is not a palindrom")
+
+fabonacci series()
+==================
+num = 0
+num_2 = 1
+limit = int(input("enter number: "))
+print(num,num_2,end=" ")
+for i in range(1,limit+1):
+    all = num + num_2
+    num = num_2
+    num_2 = all
+    print(all,end=" ")
+
+
+calculator()
+============
+
+n1 = int(input(""))
+n2 = int(input(""))
+operation = int(input("\n1.add \n2.sub \n3.multiply \n4.division \n5.power: "))
+if operation == 1:
+                print(n1 + n2)
+if operation == 2:
+                print(n1 - n2)
+if operation == 3:
+                print(n1 * n2)
+if operation == 4:
+                print(n1 / n2)
+if operation == 5:
+                print(n1 ** n2)
+
+
+table()
+=======
+table = int(input("enter number: "))
+for i in range(1,101):
+    print(f"{table} x {i} = {table*i}")
+
+
+perfect number()
+================
+n = int(input("enter number: "))
+sum = 0
+for i in range(1,n):
+    if n%i == 0:
+        sum += i
+if sum == n:
+    print(f"{n} is a perfect number")
+else:
+    print(f"{n} is not a perfect number")
+
+ 
+
+
+"""    
+
+details = {"name":"sanjay kumar",
+         "Aadhar":775204579367,
+         "pan":7543326867668798,
+         "atm pin":3530,
+         "balance":500000}
+print("********************WELCOME TO ROCKYS BANK******************")
+pin = int(input("enter your pin: "))
+
+    
+    
+
+
+
+
+
+
+
+
+                
